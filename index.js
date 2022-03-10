1a25aa6d209cc0e047376a51173e90eee892a63f
jesus bohorquez <bohorquez866@gmail.com>
2022-03-10 15:55:41 -0400

    final refactoring

--- index.js
+++ index.js
@@ -27,20 +27,24 @@ const $logo = document.querySelector(".logo");
 
 if (($burgerMenu, $mobileMenu, $logo)) {
   $burgerMenu.addEventListener("click", () => {
-    if ($burgerMenu.classList.contains("active")) {
-      $burgerMenu.classList.remove("active");
-      $burgerMenu.querySelector("img").src = `./images/icon-hamburger.svg`;
-      $logo.querySelector("img").src = `./images/logo-bookmark.svg`;
+    checkStatus(
+      $burgerMenu,
 
-      $body.style.overflow = "visible";
-      $mobileMenu.classList.remove("active");
-    } else {
-      $burgerMenu.querySelector("img").src = `./images/icon-close.svg`;
-      $logo.querySelector("img").src = `./images/logo-bookmark-all-white.svg`;
-      $burgerMenu.classList.add("active");
-      $mobileMenu.classList.add("active");
-      $body.style.overflow = "hidden";
-    }
+      () => {
+        $burgerMenu.querySelector("img").src = `./images/icon-hamburger.svg`;
+        $logo.querySelector("img").src = `./images/logo-bookmark.svg`;
+        $body.style.overflow = "visible";
+        $mobileMenu.classList.remove("active");
+      },
+
+      () => {
+        $burgerMenu.querySelector("img").src = `./images/icon-close.svg`;
+        $logo.querySelector("img").src = `./images/logo-bookmark-all-white.svg`;
+        $burgerMenu.classList.add("active");
+        $mobileMenu.classList.add("active");
+        $body.style.overflow = "hidden";
+      }
+    );
   });
 }
 
@@ -70,9 +74,8 @@ if ($featureTabHead && $featureTabContent) {
 //* FAQ Section
 
 const $faqHeads = document.querySelectorAll(".faqs .faq-head");
-
 if ($faqHeads) {
-  $faqHeads.forEach((el) => {
+  $faqHeads.forEach((el) =>
     el.addEventListener("click", (e) => {
       thisElement = e.target;
       const $faqBody = document.querySelectorAll(".faqs .faq-body");
@@ -81,14 +84,12 @@ if ($faqHeads) {
       removeAll($faqHeads);
       checkStatus(thisElement);
       removeAll($faqBody, (el) => (el.style.maxHeight = "0"));
-
       checkStatus(
         $faqContent,
         () => ($faqContent.style.maxHeight = "0"),
         () => ($faqContent.style.maxHeight = `${$faqContent.scrollHeight}px`)
       );
-
-      checkStatus($faqContent);
-    });
-  });
+      // checkStatus($faqContent);
+    })
+  );
 }
